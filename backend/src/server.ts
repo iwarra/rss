f944bcc100ee5472b9fdb322ec0611e533fb8382
Iwarra <josipovic.ivona@gmail.com>
2026-08-02 19:18:12 +0200

    New utils and cleanup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,17 @@
 import "@/rss/index";
 import { getFeedsFromDatabase, getItems, insertArticle } from "@/rss/index";
 import app from "@/routes/link";
+import { getProcessedArticles } from "./utils/getProcessedArticles";
+import { getArticlesToPersist } from "./utils/getArticlesToPersist";
 
-//   // ON SCHEDULED (every x minutes)
-//   // OPTIMAL: ON SCHEDULED BASED ON FEED (every x minute)
-await getFeedsFromDatabase();
-// feeds.forEach(async (feed) => {
-//   const items = await getItems(feed);
-
-//   items.forEach((item) => {
-//     const lala = (item: string) => true;
-//     // Vector filter
-//     const isRelevant = lala("item");
-//     if (isRelevant) addToBatchInsert() (then that will insertArticle(item) in a batch)
-//   });
-// });
+// ON SCHEDULED (every x minutes)
+// OPTIMAL: ON SCHEDULED BASED ON FEED (every x minute)
+const [feed] = await getFeedsFromDatabase("CERT-SE.");
+if (!feed) throw new Error("CERT-SE. feed was not found.");
+const items = await getItems(feed);
+const processedArticles = await getProcessedArticles(items);
+const presistantArticles = getArticlesToPersist(items, processedArticles);
+presistantArticles.forEach((article) => insertArticle(article));
 
 Bun.serve({
   port: 3000,
